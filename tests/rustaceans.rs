a56a749fb9d1051cd30ec2047418e747f3fad06d
Fernando Tomio <nandotomio@gmail.com>
2023-11-06 22:12:09 -0300

    test: crates endpoints

--- tests/rustaceans.rs
+++ tests/rustaceans.rs
@@ -1,43 +1,25 @@
 use reqwest::{blocking::Client, StatusCode};
-use rocket::serde::json::{serde_json::json, Value};
+use serde_json::{json, Value};
 
-fn create_test_rustacean(client: &Client) -> Value {
-  let response = client.post("http://localhost:8000/rustaceans")
-    .json(&json!({
-      "name": "John Doe",
-      "email": "johndoe@test.com" 
-    }))
-    .send()
-    .unwrap();
-  assert_eq!(response.status(), StatusCode::CREATED);
-
-  response.json().unwrap()
-}
-
-fn delete_test_rustacean(client: &Client, rustacean: Value) {
-  let response = client.delete(format!("http://localhost:8000/rustaceans/{}", rustacean["id"]))
-    .send()
-    .unwrap();
-  assert_eq!(response.status(), StatusCode::NO_CONTENT);
-}
+pub mod common;
 
 #[test]
 fn test_get_rustaceans() {
   // Setup
   let client = Client::new();
-  let rustacean1 = create_test_rustacean(&client);
-  let rustacean2 = create_test_rustacean(&client);
+  let rustacean1 = common::create_test_rustacean(&client);
+  let rustacean2 = common::create_test_rustacean(&client);
 
   // Test
-  let response = client.get("http://localhost:8000/rustaceans").send().unwrap();
+  let response = client.get(format!("{}/rustaceans", common::APP_HOST)).send().unwrap();
   assert_eq!(response.status(), StatusCode::OK);
   let json = response.json::<Value>().unwrap();
   assert!(json.as_array().unwrap().contains(&rustacean1));
   assert!(json.as_array().unwrap().contains(&rustacean2));
 
   // Teardown
-  delete_test_rustacean(&client, rustacean1);
-  delete_test_rustacean(&client, rustacean2);
+  common::delete_test_rustacean(&client, rustacean1);
+  common::delete_test_rustacean(&client, rustacean2);
 }
 
 #[test]
@@ -46,7 +28,7 @@ fn test_create_rustaceans() {
   let client = Client::new();
 
   // Test
-  let response = client.post("http://localhost:8000/rustaceans")
+  let response = client.post(format!("{}/rustaceans", common::APP_HOST))
     .json(&json!({
       "name": "John Doe",
       "email": "johndoe@test.com" 
@@ -64,17 +46,17 @@ fn test_create_rustaceans() {
   }));
 
   // Teardown
-  delete_test_rustacean(&client, rustacean);
+  common::delete_test_rustacean(&client, rustacean);
 }
 
 #[test]
 fn test_view_rustaceans() {
   // Setup
   let client = Client::new();
-  let rustacean = create_test_rustacean(&client);
+  let rustacean = common::create_test_rustacean(&client);
 
   // Test
-  let response = client.get(format!("http://localhost:8000/rustaceans/{}", rustacean["id"]))
+  let response = client.get(format!("{}/rustaceans/{}", common::APP_HOST, rustacean["id"]))
     .send()
     .unwrap();
   assert_eq!(response.status(), StatusCode::OK);
@@ -88,17 +70,17 @@ fn test_view_rustaceans() {
   }));
 
   // Teardown
-  delete_test_rustacean(&client, rustacean);
+  common::delete_test_rustacean(&client, rustacean);
 }
 
 #[test]
 fn test_update_rustaceans() {
   // Setup
   let client = Client::new();
-  let rustacean = create_test_rustacean(&client);
+  let rustacean = common::create_test_rustacean(&client);
 
   // Test
-  let response = client.put(format!("http://localhost:8000/rustaceans/{}", rustacean["id"]))
+  let response = client.put(format!("{}/rustaceans/{}", common::APP_HOST, rustacean["id"]))
     .json(&json!({
       "name": "Johnny Doe",
       "email": "johnnydoe@test.com" 
@@ -116,17 +98,17 @@ fn test_update_rustaceans() {
   }));
 
   // Teardown
-  delete_test_rustacean(&client, rustacean);
+  common::delete_test_rustacean(&client, rustacean);
 }
 
 #[test]
 fn test_delete_rustaceans() {
   // Setup
   let client = Client::new();
-  let rustacean = create_test_rustacean(&client);
+  let rustacean = common::create_test_rustacean(&client);
 
   // Test
-  let response = client.delete(format!("http://localhost:8000/rustaceans/{}", rustacean["id"]))
+  let response = client.delete(format!("{}/rustaceans/{}", common::APP_HOST, rustacean["id"]))
     .send()
     .unwrap();
   assert_eq!(response.status(), StatusCode::NO_CONTENT);
