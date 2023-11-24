a0626c524ec645e56be8cbedc852d04f0ee1748e
Fernando Tomio <nandotomio@gmail.com>
2023-11-24 19:16:55 -0300

    feat: add users, roles and user roles models

--- src/models.rs
+++ src/models.rs
@@ -1,26 +1,26 @@
 use chrono::NaiveDateTime;
-use diesel::*;
+use diesel::prelude::*;
 use crate::schema::*;
-use serde::*;
+use serde::{Serialize, Deserialize};
 
-#[derive(Queryable, AsChangeset, Deserialize, Serialize)]
+#[derive(Queryable, AsChangeset, Serialize, Deserialize)]
 pub struct Rustacean {
     #[serde(skip_deserializing)]
     pub id: i32,
     pub name: String,
     pub email: String,
     #[serde(skip_deserializing)]
-    pub created_at: NaiveDateTime,
+    pub created_at: NaiveDateTime
 }
 
 #[derive(Insertable, Deserialize)]
-#[diesel(table_name = rustaceans)]
+#[diesel(table_name=rustaceans)]
 pub struct NewRustacean {
     pub name: String,
     pub email: String,
 }
 
-#[derive(Queryable, AsChangeset, Deserialize, Serialize)]
+#[derive(Queryable, Serialize, Deserialize)]
 pub struct Crate {
     #[serde(skip_deserializing)]
     pub id: i32,
@@ -30,15 +30,61 @@ pub struct Crate {
     pub version: String,
     pub description: Option<String>,
     #[serde(skip_deserializing)]
-    pub created_at: NaiveDateTime,
+    pub created_at: NaiveDateTime
 }
 
 #[derive(Insertable, Deserialize)]
-#[diesel(table_name = crates)]
+#[diesel(table_name=crates)]
 pub struct NewCrate {
     pub rustacean_id: i32,
     pub code: String,
     pub name: String,
     pub version: String,
-    pub description: Option<String>,
+    pub description: Option<String>
+}
+
+#[derive(Queryable, Debug, Identifiable)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    pub created_at: NaiveDateTime
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=users)]
+pub struct NewUser {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Queryable, Debug, Identifiable)]
+pub struct Role {
+    pub id: i32,
+    pub code: String,
+    pub name: String,
+    pub created_at: NaiveDateTime
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=roles)]
+pub struct NewRole {
+    pub code: String,
+    pub name: String,
+}
+
+#[derive(Queryable, Associations, Identifiable, Debug)]
+#[diesel(belongs_to(User))]
+#[diesel(belongs_to(Role))]
+#[diesel(table_name=users_roles)]
+pub struct UserRole {
+    pub id: i32,
+    pub user_id: i32,
+    pub role_id: i32,
+}
+#[derive(Insertable)]
+#[diesel(table_name=users_roles)]
+pub struct NewUserRole {
+    pub user_id: i32,
+    pub role_id: i32,
 }
