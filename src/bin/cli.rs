3ef2dd70565dba97a4c34006ec7b8c0eab1aa634
Fernando Tomio <nandotomio@gmail.com>
2023-11-27 08:32:57 -0300

    feat: add CLI commands

---
+++ src/bin/cli.rs
@@ -0,0 +1,50 @@
+use clap::{Command, Arg, value_parser};
+
+extern crate cr8s;
+
+#[tokio::main]
+async fn main() {
+  let matches = Command::new("Cr8s")
+    .about("Cr8s commands")
+    .arg_required_else_help(true)
+    .subcommand(
+      Command::new("users")
+        .about("Users management")
+        .arg_required_else_help(true)
+        .subcommand(
+          Command::new("create")
+            .about("Create a new user")
+            .arg_required_else_help(true)
+            .arg(Arg::new("username").required(true))
+            .arg(Arg::new("password").required(true))
+            .arg(Arg::new("roles").required(true).num_args(1..).value_delimiter(','))
+        )
+        .subcommand(
+          Command::new("list")
+            .about("List existing users")
+        )
+        .subcommand(
+          Command::new("delete")
+            .about("Delete user by ID")
+            .arg_required_else_help(true)
+            .arg(Arg::new("id").required(true).value_parser(value_parser!(i32)))
+        )
+    )
+    .get_matches();
+
+  match matches.subcommand() {
+    Some(("users", sub_matches)) => match sub_matches.subcommand() {
+      Some(("create", sub_matches)) => cr8s::commands::create_users(
+        sub_matches.get_one::<String>("username").unwrap().to_owned(),
+        sub_matches.get_one::<String>("password").unwrap().to_owned(),
+        sub_matches.get_many::<String>("roles").unwrap().map(|v| v.to_owned()).collect()
+      ).await,
+      Some(("list", _)) => cr8s::commands::list_users().await,
+      Some(("delete", sub_matches)) => cr8s::commands::delete_user(
+        sub_matches.get_one::<i32>("id").unwrap().to_owned()
+      ).await,
+      _ => {}
+    },
+    _ => {}
+  }
+}
