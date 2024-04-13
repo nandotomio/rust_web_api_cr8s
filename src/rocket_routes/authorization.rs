fcd1295c9eb69b79248f19f398bf715b8af9677c
Fernando Tomio <nandotomio@gmail.com>
2024-04-13 18:31:07 -0300

    feat: implement cache for session id

--- src/rocket_routes/authorization.rs
+++ src/rocket_routes/authorization.rs
@@ -1,18 +1,36 @@
-use crate::rocket_routes::{DbConn, CacheConn, server_error};
-use crate::repositories::UserRepository;
 use crate::auth::{authorize_user, Credentials};
-use rocket::serde::json::{json, Json, Value};
+use crate::repositories::UserRepository;
+use crate::rocket_routes::{server_error, CacheConn, DbConn};
+use rocket::http::Status;
 use rocket::response::status::Custom;
+use rocket::serde::json::{json, Json, Value};
+use rocket_db_pools::deadpool_redis::redis::AsyncCommands;
 use rocket_db_pools::Connection;
 
-#[rocket::post("/login", format = "json", data="<credentials>")]
-pub async fn login(mut db: Connection<DbConn>, mut cache: Connection<CacheConn>, credentials: Json<Credentials>) -> Result<Value, Custom<Value>> {
-  UserRepository::find_by_username(&mut db, &credentials.username).await
-    .map(|user| {
-      if let Ok(token) = authorize_user(&user, credentials.into_inner()) {
-        return json!(token);
-      }
-      json!("Unauthorized")
-    })
-    .map_err(|e| server_error(e.into()))
+#[rocket::post("/login", format = "json", data = "<credentials>")]
+pub async fn login(
+    mut db: Connection<DbConn>,
+    mut cache: Connection<CacheConn>,
+    credentials: Json<Credentials>,
+) -> Result<Value, Custom<Value>> {
+    let user = UserRepository::find_by_username(&mut db, &credentials.username)
+        .await
+        .map_err(|e| {
+          match e {
+            diesel::result::Error::NotFound => Custom(Status::Unauthorized, json!("Wrong credentials")),
+            _ => server_error(e.into()),
+          }
+        })?;
+
+    let session_id = authorize_user(&user, credentials.into_inner())
+        .map_err(|_| Custom(Status::Unauthorized, json!("Wrong credentials")))?;
+
+    cache
+        .set_ex::<String, i32, ()>(format!("sessions/{}", session_id), user.id, 3 * 60 * 60)
+        .await
+        .map_err(|e| server_error(e.into()))?;
+
+    Ok(json!({
+      "token": session_id
+    }))
 }
